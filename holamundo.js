96def0c659a5820563323630d2727edf19b95798
Omaira85 <ladyotavo@gmail.com>
2017-07-12 18:16:05 -0500

    Create holamundo.js

---
+++ holamundo.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/sergio', function (req, res) {
+  res.send('Hello Sergio!');
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
+});
